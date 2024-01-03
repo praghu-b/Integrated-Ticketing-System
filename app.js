a969cfd810ea2fa7078737c8dafe378008b01fe6
praghub <prakashbalan555@gmail.comgit config --global --listgit config --global user.email prakashbalan555@gmail.com>
2024-01-03 23:57:35 +0530

    feat: vercel.json

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.set('view engine', 'ejs')
-app.use('/',express.static('public'))
+app.use('/',express.static(__dirname + 'public'))
 app.use('/node_modules', express.static('node_modules'))
-app.set('views','views')
+app.set('views',__dirname + 'views')
 
 app.use('/verify', verifyID)
 
