3d30979f72d1d31afb661ef32f7b3f1ae15f6b8f
praghub <prakashbalan555@gmail.comgit config --global --listgit config --global user.email prakashbalan555@gmail.com>
2024-01-04 00:25:26 +0530

    fix: app.js, index.ejs, success.ejs

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'))
 app.use('/',express.static(__dirname + '/public'))
 app.use('/node_modules', express.static(__dirname + '/node_modules'))
 app.set('views',__dirname + '/views')
